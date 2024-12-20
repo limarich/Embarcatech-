8229a829dfc6903b5a245d394287b209f2c66c53
Jônatas da Silva <jonatas.silva.roots@gmail.com>
2024-12-20 17:23:59 -0300

    :sparkles: feat: insere conversor para milímetros
    Insere a função de conversão para milímetros quadrados.

--- libs/area.c
+++ libs/area.c
@@ -11,24 +11,45 @@
 #define KM2_PARA_M2 1000000.0
 #define HA_PARA_M2 10000.0
 
+/* Função de conversão de outras unidades de área para metros milimetro quadrado */
+float convert_to_millimeter(float value, char *unit)
+{
+  if (strcmp(unit, "cm") == 0)
+  {
+    return value * 100;
+  }
+  else if (strcmp(unit, "m") == 0)
+  {
+    return value * 1000000.0;
+  }
+  else if (strcmp(unit, "km") == 0)
+  {
+    return value * 1000000000000.0;
+  }
+  else if (strcmp(unit, "ha") == 0)
+  {
+    return value * 10000000000.0;
+  }
+}
+
 /* Função de conversão de outras unidades de área para metros quadrados */
 float convert_to_meters(float value, const char *unit)
 {
   if (strcmp(unit, "mm") == 0)
   {
-    return value * MM2_PARA_M2;
+    return value * 0.000001;
   }
   else if (strcmp(unit, "cm") == 0)
   {
-    return value * CM2_PARA_M2;
+    return value * 0.0001;
   }
   else if (strcmp(unit, "km") == 0)
   {
-    return value * KM2_PARA_M2;
+    return value * 1000000.0;
   }
   else if (strcmp(unit, "ha") == 0)
   {
-    return value * HA_PARA_M2;
+    return value * 10000.0;
   }
 }
 

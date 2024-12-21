6e9e0ef99b8681f55b995b87e661aa70b91a9f7d
Josephbm <johbr85@gmail.com>
2024-12-21 13:57:44 -0300

    Tratasse de um código de Conversão de Temperaturas, onde a parte lógica está localizada dentro do arquivo 'conversor.c' e a lib/header está no arquivo 'conversor.h'.

---
+++ conversor.h
@@ -0,0 +1,9 @@
+#ifndef CONVERSOR_H
+#define CONVERSOR_H
+
+float converterTemperatura(float temp, char fromUnit, char toUnit);
+
+#endif
+
+// isto é apenas um header file para definir a função para
+// converter as temperaturas.

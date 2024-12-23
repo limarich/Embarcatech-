39dc09c6f67a06c132f0b77437b33a7fceefe77d
Richard <richard.esclima@gmail.com>
2024-12-23 14:29:35 -0300

    feat: adapta o conversor de unidade de tempo para se comportar como biblioteca

--- main.c
+++ main.c
@@ -1,21 +1,8 @@
 #include "conversor_mps_kmh.h"
+#include "conversor_unidade_tempo.h"
 #include <stdio.h>
 
 int main()
 {
-    float mps, kmh;
-
-    printf("Digite a velocidade em metros por segundo (m/s): ");
-
-    // Valida a entrada do usuário
-    if (scanf("%f", &mps) != 1)
-    {
-        printf("Entrada inválida! Por favor, insira um número.\n");
-        return 1;
-    }
-
-    kmh = metros_por_segundo_para_quilometros_por_hora(mps);
-
-    printf("%.2f metros por segundo equivalem a %.2f quilômetros por hora.\n", mps, kmh);
     return 0;
 }
